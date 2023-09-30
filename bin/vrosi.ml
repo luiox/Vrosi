df2035fbd79cd7c2d20b3c9fd32920d7c6052581
Canrad <canrad7@163.com>
2023-09-30 16:18:17 +0800

    对单文件的统计

--- bin/vrosi.ml
+++ bin/vrosi.ml
@@ -1,25 +1,90 @@
 open Printf
-open Str
 
-let filename = "test.txt"
-let message = "Hello!"
-
-let match_regex pattern str =
-    let regex = regexp pattern in
+let count_lines filename =
+    let ic = open_in filename in
+    let rec count_lines_helper count empty_count comment_count in_comment =
         try
-          ignore (search_forward regex str 0);
-          true
-        with Not_found -> false
+          let line = input_line ic in
+          let trimmed_line = String.trim line in
+              if String.length trimmed_line = 0 then
+                count_lines_helper count (empty_count + 1) comment_count
+                  in_comment
+              else if in_comment then
+                let end_comment_index =
+                    try String.index trimmed_line '*' with Not_found -> -1
+                in
+                    if
+                      end_comment_index >= 0
+                      && end_comment_index < String.length trimmed_line - 1
+                      && String.sub trimmed_line end_comment_index 2 = "*/"
+                    then
+                      count_lines_helper count empty_count (comment_count + 1)
+                        false
+                    else
+                      count_lines_helper count empty_count (comment_count + 1)
+                        true
+              else if
+                String.length trimmed_line >= 2
+                && String.sub trimmed_line 0 2 = "//"
+              then
+                count_lines_helper count empty_count (comment_count + 1)
+                  in_comment
+              else if
+                String.length trimmed_line >= 2
+                && String.sub trimmed_line 0 2 = "/*"
+              then
+                let end_comment_index =
+                    try String.index trimmed_line '*' with Not_found -> -1
+                in
+                    if
+                      end_comment_index >= 0
+                      && end_comment_index < String.length trimmed_line - 1
+                      && String.sub trimmed_line end_comment_index 2 = "*/"
+                    then
+                      count_lines_helper count empty_count (comment_count + 1)
+                        false
+                    else
+                      count_lines_helper count empty_count (comment_count + 1)
+                        true
+              else
+                count_lines_helper (count + 1) empty_count comment_count
+                  in_comment
+        with End_of_file ->
+          close_in ic;
+          (filename, count, empty_count, comment_count)
+    in
+        count_lines_helper 0 0 0 false
+
+let print_usage =
+   print_endline "Unknown!";
+   print_endline "Usage: Vrosi [f/d] [files/directories]"
+
+let process_command_line_args args =
+    match Array.length args with
+    | n when n < 3 -> print_usage
+    | _ ->
+      let option = args.(1) in
+      match option with
+      | "f" ->
+        begin
+        let filenames = Array.sub args 2 (Array.length args - 2) in
+            Array.iter
+              (fun filename ->
+                let filepath = Printf.sprintf "%s" filename in
+                let name, lines, empty_lines, comment_lines =
+                    count_lines filepath
+                in
+                    printf "File: %s\n" name;
+                    printf "Code Lines: %d\n" lines;
+                    printf "Empty Lines: %d\n" empty_lines;
+                    printf "Comment Lines: %d\n" comment_lines;
+                    printf "\n")
+              filenames
+         end
+      | "d" ->()
+      | _ -> ()
 
 let () =
-    let args_num = Array.length Sys.argv - 1 in
-        Printf.printf "Number of arguments:%i\n" args_num;
-        (* 遍历命令行参数 *)
-        for i = 1 to args_num do
-          Printf.printf "Argument %i:%s\n" i Sys.argv.(i)
-        done;
-        Util.write_file filename message;
-        Util.print_file filename;
-        Util.print_all_files ".";
-        let t = match_regex "123" "?2?" in
-            printf "%b" t
+    let args = Sys.argv in
+        process_command_line_args args
+
