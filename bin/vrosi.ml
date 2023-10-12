27f83a8c775bee8ff54604e7b79a15c475e0854c
Canrad <1517807724@qq.com>
2023-10-12 15:42:56 +0800

    完成代码优化

--- bin/vrosi.ml
+++ bin/vrosi.ml
@@ -1,44 +1,53 @@
 open Printf
 
+(* determine whether a string contains a specific string *)
 let contains_substring str substring =
-  let substring_length = String.length substring in
-  let rec loop i =
-    if i + substring_length > String.length str then
-      false
-    else if String.sub str i substring_length = substring then
-      true
-    else
-      loop (i + 1)
-  in
-  loop 0
+    let substring_length = String.length substring in
+    let rec loop i =
+        if i + substring_length > String.length str then
+          false
+        else if String.sub str i substring_length = substring then
+          true
+        else
+          loop (i + 1)
+    in
+        loop 0
 
+(* new cont lines *)
 let count_lines filename =
-  let ic = open_in filename in
-  let rec count_lines_helper count empty_count comment_count in_comment =
-      try
-        let line = input_line ic in
-        let trimmed_line = String.trim line in
-          if contains_substring trimmed_line "//" ||
-            (contains_substring trimmed_line "/*" &&
-            contains_substring trimmed_line "*/") then
-            count_lines_helper count empty_count (comment_count+1) in_comment
-          else if in_comment then
-            if contains_substring trimmed_line "*/" then
-              count_lines_helper count empty_count (comment_count+1) false
-            else
-              count_lines_helper (count + 1) empty_count comment_count in_comment
-          else if contains_substring trimmed_line "/*" then
-            count_lines_helper count empty_count (comment_count+1) true
-          else if String.length trimmed_line = 0 then
-            count_lines_helper count (empty_count+1) comment_count in_comment
-          else
-            count_lines_helper (count+1) empty_count comment_count in_comment
-      with End_of_file ->
-        close_in ic;
-        (filename, count, empty_count, comment_count)
-  in
-      count_lines_helper 0 0 0 false
+    let ic = open_in filename in
+    let rec count_lines_helper count empty_count comment_count in_comment =
+        try
+          let line = input_line ic in
+          let trimmed_line = String.trim line in
+              if in_comment then
+                if contains_substring trimmed_line "*/" then
+                  count_lines_helper count empty_count (comment_count + 1) false
+                else
+                  count_lines_helper count empty_count (comment_count + 1)
+                    in_comment
+              else if
+                contains_substring trimmed_line "//"
+                || contains_substring trimmed_line "/*"
+                   && contains_substring trimmed_line "*/"
+              then
+                count_lines_helper count empty_count (comment_count + 1)
+                  in_comment
+              else if contains_substring trimmed_line "/*" then
+                count_lines_helper count empty_count (comment_count + 1) true
+              else if String.length trimmed_line = 0 then
+                count_lines_helper count (empty_count + 1) comment_count
+                  in_comment
+              else
+                count_lines_helper (count + 1) empty_count comment_count
+                  in_comment
+        with End_of_file ->
+          close_in ic;
+          (filename, count, empty_count, comment_count)
+    in
+        count_lines_helper 0 0 0 false
 
+(* old count lines *)
 (* let count_lines filename =
     let ic = open_in filename in
     let rec count_lines_helper count empty_count comment_count in_comment =
@@ -108,7 +117,7 @@ let count_and_print_file_info filename =
         printf "Comment Lines: %d\n" comment_lines;
         printf "\n"
 
-(* recursively traverse a directory and apply a handler to each file. *)
+(* recursively traverse a directory and apply a handler to each file *)
 let rec traverse_directory path handler =
     let files = Sys.readdir path in
         Array.iter
@@ -117,12 +126,11 @@ let rec traverse_directory path handler =
                 if Sys.is_directory full_path then
                   traverse_directory full_path handler
                 else
-                  handler full_path
-          )
+                  handler full_path)
           files
 
 (* handle command line agruments *)
-(* arccording to different options, choose different way to handle. *)
+(* arccording to different options, choose different way to handle *)
 let process_command_line_args args =
     match Array.length args with
     | n when n < 3 -> print_usage
